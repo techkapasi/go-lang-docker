718301227869d8d39fbd182064ce96bf8d047274
techkapasi <techkapasi@gmail.com>
2017-06-14 16:34:52 +0500

    Updated HTML Template Code with Dynamic Data

--- webtemplate/main.go
+++ webtemplate/main.go
@@ -1,48 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"strings"
 )
 
-type dataSender struct {
-	username string
-}
+func runHTMLTemplate(w http.ResponseWriter, r *http.Request) {
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()               // parse arguments, you have to call this by yourself
-	fmt.Println("Form", r.Form) // print form information in server side
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+	data := struct {
+		Name     string
+		Company  string
+	}{
+		"Hasan Kapasi",
+		"10Pearls",
 	}
 
-	data := &dataSender{}
-	data.username = "World!"
-
-	// tmpl is the HTML template that drives the user interface.
 	var tmpl = template.Must(template.New("tmpl").Parse(`
-	<!DOCTYPE html><html><body><center>
-		<h1>Hello {.username}</h1>
-	</center></body></html>
+	<!DOCTYPE html>
+	<html>
+		<body>
+			<center>
+				<h1>Hello From {{ .Name }} ~ {{ .Company }}</h1>
+			</center>
+		</body>
+	</html>
 	`))
 
-	err := tmpl.Execute(w, r)
+
+	err := tmpl.Execute(w, data)
 	if err != nil {
 		log.Print(err)
 	}
-
-	//fmt.Fprintf(w, "Hello World!") // send data to client side
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       // set router
+	http.HandleFunc("/", runHTMLTemplate)       // set router
 	err := http.ListenAndServe(":8080", nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
