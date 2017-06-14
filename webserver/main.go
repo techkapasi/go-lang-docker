f22cfe0e8d87eccc991e52ab9a2a460a2d483a10
Asjad Azeem <asjad.azeem@tenpearls.com>
2017-06-14 13:14:28 +0500

    Add template in webserver
    Signed-off-by: Asjad Azeem <asjad.azeem@tenpearls.com>

--- webserver/main.go
+++ webserver/main.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"html/template"
 	"log"
+	"net/http"
+	"strings"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! ~10Pearls")
+	r.ParseForm()               // parse arguments, you have to call this by yourself
+	fmt.Println("Form", r.Form) // print form information in server side
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+
+	// tmpl is the HTML template that drives the user interface.
+	var tmpl = template.Must(template.New("tmpl").Parse(`
+	<!DOCTYPE html><html><body><center>
+		<h1>Hello</h1>
+	</center></body></html>
+	`))
+
+	err := tmpl.Execute(w, r)
+	if err != nil {
+		log.Print(err)
+	}
+
+	//fmt.Fprintf(w, "Hello World!") // send data to client side
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)
-	err := http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", sayhelloName)       // set router
+	err := http.ListenAndServe(":8080", nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
